66de4926569acf258a34b2d4aef9f62bf0a5d6fc
peter ciprian olah <peter.ciprian.olah@suitsolutions.eu>
2018-09-25 10:00:02 +0200

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,49 @@
+import { NgModule, Component, Input, Output, ElementRef, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FileUploadComponent),
+      multi: true
+    }
+  ]
+})
+export class FileUploadComponent implements ControlValueAccessor {
+
+  constructor() { }
+
+  selectedFileName: string = null;
+  @Input() showFileNameInput: boolean;
+  @Input() uploadButtonText: string;
+
+  writeValue(value: any) {
+    //Handle write value
+  }
+  propagateChange = (_: any) => { };
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+  registerOnTouched() { }
+
+  changeListener($event): void {
+    // debugger; // uncomment this for debugging purposes
+    this.readThis($event.target);
+  }
+  readThis(inputValue: any): void {
+    // debugger; // uncomment this for debugging purposes
+    var file: File = inputValue.files[0];
+    var myReader: FileReader = new FileReader();
+
+    myReader.onloadend = (e) => {
+      this.propagateChange(myReader.result);
+      this.selectedFileName = file.name;
+    }
+    myReader.readAsDataURL(file);
+  }
+}
